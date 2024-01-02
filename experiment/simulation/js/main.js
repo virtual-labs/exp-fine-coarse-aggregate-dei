397d265076090b7a9bfcdc362a7bc96211b3a1a5
Varun Pal Singh <singhvarun928@gmail.com>
2024-01-02 10:23:05 +0530

    started exp

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -1,1 +1,6 @@
 //Your JavaScript goes in here
+const nextToStep1 = document.querySelector("#nextToStep1");
+
+nextToStep1.addEventListener("click", (evt) => {
+    window.location.href = "./steps-files/step1.html";
+})
