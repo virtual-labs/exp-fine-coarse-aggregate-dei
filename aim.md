c02372e0d0f3c7cd999d40dab7245be37b3e4da0
vleadadmin <admin@vlabs.ac.in>
2026-03-23 06:39:59 +0000

    https://virtual-labs.github.io/virtual-labs/exp-fine-coarse-aggregate-dei click on the link to test your code.

---
+++ aim.md
@@ -0,0 +1,1 @@
+#### To determine the soundness of fine and coarse aggregate.
---
+++ simulation/js/FineAggregate/step11.js
@@ -0,0 +1,8 @@
+function gif11a() {
+    document.getElementById("nextt").style.visibility = "hidden";
+    document.getElementById("next").style.visibility = "hidden";
+    document.getElementById("arrow").style.visibility = "hidden";
+    document.getElementById("empty").style.visibility = "hidden";
+    document.getElementById("full").style.visibility = "visible";
+    document.getElementById("finish").style.visibility = "visible";
+}
